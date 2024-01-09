a157d2a6b3e8ac634ae04148d70c70024ac12666
Eray <Eray@ERAY-MONSTER>
2024-01-09 23:09:14 +0300

    Proje dosyası ekle.

---
+++ C#RANDOM SAYİ/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int isayi, bsayi, endsayi;
+
+        Console.WriteLine("Kaç tane random sayı istiyorsun");
+        isayi = int.Parse(Console.ReadLine());
+        if (isayi >0)
+        {
+       
+
+        Console.WriteLine("başlangıç sayını gir");
+        bsayi = int.Parse(Console.ReadLine());
+        Console.WriteLine("bitiş sayını gir");
+        endsayi = int.Parse(Console.ReadLine());
+
+            if(endsayi-bsayi>isayi)
+            {
+            
+             Random rnd = new Random();
+        int[] sayilar = new int[isayi];
+
+        for (int i = 0; i < isayi;)
+        {
+            int eray = rnd.Next(bsayi, endsayi + 1);
+            bool benzersiz = true;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (sayilar[j] == eray)
+                {
+                    benzersiz = false;
+                    break;
+                }
+            }
+
+            if (benzersiz)
+            {
+                sayilar[i] = eray;
+                i++;
+            }
+        }
+
+        Array.Sort(sayilar);
+
+        foreach (int sayi in sayilar)
+        {
+            Console.WriteLine(sayi);
+        }
+
+            }
+            else
+            {
+                Console.WriteLine($"Random Sayi miktarını En fazla {endsayi-bsayi} giriniz. Yada bitiş sayınızı büyütünüz");
+            }
+
+       
+        }
+        else { Console.WriteLine("Hatalı giriş yaptınız.0 dan büyük değer giriniz."); }
+        Console.ReadKey();
+    }
+}
---
+++ C#RANDOM SAYİ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("C#RANDOM SAYİ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C#RANDOM SAYİ")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("42be83e3-2885-40f4-ae84-1402eb20fe49")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
